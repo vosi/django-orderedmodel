e5b88e839486bcc1bc10c2f2f8eea033097e9c89
Vladimir Tartynskyi <fon.vosi@gmail.com>
2011-11-16 17:24:30 +0200

    fix 1.4 (no backward)

--- orderedmodel/admin.py
+++ orderedmodel/admin.py
@@ -7,45 +7,50 @@ from django.utils.translation import ugettext as _
 
 
 class OrderedModelAdmin(admin.ModelAdmin):
-  ordering = ['order']
-  exclude = ['order']
+    ordering = ['order']
+    exclude = ['order']
 
-  def get_urls(self):
-    my_urls = patterns('',
-        (r'^(?P<pk>\d+)/move_up/$', self.admin_site.admin_view(self.move_up)),
-        (r'^(?P<pk>\d+)/move_down/$', self.admin_site.admin_view(self.move_down)),
-    )
-    return my_urls + super(OrderedModelAdmin, self).get_urls()
+    def get_urls(self):
+        my_urls = patterns('',
+            (r'^(?P<pk>\d+)/move_up/$', self.admin_site.admin_view(self.move_up)),
+            (r'^(?P<pk>\d+)/move_down/$', self.admin_site.admin_view(self.move_down)),
+        )
+        return my_urls + super(OrderedModelAdmin, self).get_urls()
 
-  def reorder(self, item):
-    button = '<a href="{{0}}/move_{{1}}"><img class="ctl arr_{{1}}" src="{0}admin/img/arrow-{{1}}.gif" border="0" alt="{{1}}" /></a>'.format(settings.STATIC_URL)
+    def reorder(self, item):
+        button = '<a href="{{0}}/move_{{1}}"><img class="ctl arr_{{1}}" src="{0}admin/img/arrow-{{1}}.gif" border="0" alt="{{1}}" /></a>'.format(
+            settings.STATIC_URL)
 
-    html = ''
-    html += button.format(item.pk, 'down')
-    html += '&nbsp;' + button.format(item.pk, 'up')
-    return html
-  reorder.allow_tags = True
-  reorder.short_description = _('Reorder')
+        html = '<style type="text/css"> '\
+               '.ctl { width: 14px; height: 12px; display: inline-block;} '\
+               '.arr_up { background: url(\'{0}admin/img/sorting-icons.gif\') -5px -50px no-repeat; } '\
+               '.arr_down { background: url(\'{0}admin/img/sorting-icons.gif\') -5px -94px no-repeat; } '\
+               '</style>'.format(settings.STATIC_URL)
+        html += button.format(item.pk, 'down')
+        html += '&nbsp;' + button.format(item.pk, 'up')
+        return html
 
-  def move_down(self, request, pk):
-    if self.has_change_permission(request):
-      item = get_object_or_404(self.model, pk=pk)
-      try:
-        next_item = self.model.objects.filter(order__gt=item.order).order_by('order')[0]
-      except IndexError:
-        pass
-      else:
-        self.model.swap(item, next_item)
-    return HttpResponseRedirect('../../')
+    reorder.allow_tags = True
+    reorder.short_description = _('Reorder')
 
-  def move_up(self, request, pk):
-    if self.has_change_permission(request):
-      item = get_object_or_404(self.model, pk=pk)
-      try:
-        prev_item = self.model.objects.filter(order__lt=item.order).order_by('-order')[0]
-      except IndexError:
-        pass
-      else:
-        self.model.swap(item, prev_item)
-    return HttpResponseRedirect('../../')
+    def move_down(self, request, pk):
+        if self.has_change_permission(request):
+            item = get_object_or_404(self.model, pk=pk)
+            try:
+                next_item = self.model.objects.filter(order__gt=item.order).order_by('order')[0]
+            except IndexError:
+                pass
+            else:
+                self.model.swap(item, next_item)
+        return HttpResponseRedirect('../../')
 
+    def move_up(self, request, pk):
+        if self.has_change_permission(request):
+            item = get_object_or_404(self.model, pk=pk)
+            try:
+                prev_item = self.model.objects.filter(order__lt=item.order).order_by('-order')[0]
+            except IndexError:
+                pass
+            else:
+                self.model.swap(item, prev_item)
+        return HttpResponseRedirect('../../')
